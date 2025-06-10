32542aa607b9585d7b5226224f402b4cc78def78
Daniel <danielchenschw@gmail.com>
2025-06-10 18:39:58 -0400

    Initialize project

---
+++ controller/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
+];
+
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    console.log("at / right now");
+    res.render("index", { title: "Dan's message board", messages: messages });
+});
+
+app.get("/new", (req, res) => {
+    console.log("at /new right now");
+    res.render("./post/postMessage");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
