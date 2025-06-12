645662282b866a175bb58d21323950c3b65166cc
Daniel <danielchenschw@gmail.com>
2025-06-11 20:30:33 -0400

    Add post request function and update styling

--- controller/app.js
+++ controller/app.js
@@ -15,6 +15,10 @@ const messages = [
     },
 ];
 
+const assetsPath = path.join(__dirname, "../public");
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
 
@@ -28,7 +32,12 @@ app.get("/new", (req, res) => {
     res.render("./post/postMessage");
 });
 
-app.post("/new", (req, res) => {});
+app.post("/new", (req, res) => {
+    messageText = req.body.message;
+    messageUser = req.body.name;
+    messages.push({ text: messageText, user: messageUser, added: new Date() });
+    res.redirect("/");
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
